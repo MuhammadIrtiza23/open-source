2dff4be6f875087cdca13c61f4ebd849a4496ada
Muhammad Irtiza <145038840+MuhammadIrtiza23@users.noreply.github.com>
2024-04-22 19:33:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+document.getElementById("hamwrapper").onclick = function() {
+    var element = document.getElementById("navDiv");
+    element.classList.toggle("navDivnew");
+}
+window.addEventListener("scroll",function(){
+    var header = this.document.querySelector("img");
+    var text = this.document.querySelector("h1");
+
+    var value = window.scrollY;
+
+    header.style.top = value* 1 + 'px';
+    text.style.top = value* -4 + 'px';
+
+})
+
+window.addEventListener("scroll",function(){
+    var header = this.document.querySelector("#hamwrapper");
+    header.classList.toggle("changebg",this.window.scrollY>0)
+
+})
